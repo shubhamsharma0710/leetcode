a57b800e4651c80b10a29b50c59be287acb20759
shubham <84614805+shubhamsharma0710@users.noreply.github.com>
2026-06-22 19:51:48 +0530

    Time: 71 ms (0.65%), Space: 8.9 MB (84.03%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+       int ans=0;
+       while(true){
+        string word="balloon";
+        for(char &c : word){
+            bool found=false;
+            for(char &ch:text){
+                if(ch==c){
+                    ch='#';
+                    found=true;
+                    break;
+                }
+            }
+            if(!found) return ans;
+        }
+        ans++;
+       } 
+    }
+};
